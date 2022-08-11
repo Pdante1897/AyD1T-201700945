42333d79f31d514f208f375a277f99c6ef53bbdf
Bryan Paez <bryanpaez.125@gmail.com>
2022-08-11 14:57:32 -0600

    Se creo el endpoint resta
    Se creo el endpoint resta para realizar una resta de dos numeros recibidos desde una peticion tipo POST

--- EndPoints/main.go
+++ EndPoints/main.go
@@ -41,6 +41,24 @@ func info(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Bryan Gerardo Paez Morales 201700945")
 }
 
+func resta(w http.ResponseWriter, r *http.Request) {
+	archivo := new(ArchivoJson)
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Error al insertar mensaje")
+		return
+	}
+	json.Unmarshal(reqBody, &archivo)
+
+	var num1, num2 int
+	num1 = archivo.Numero1
+	num2 = archivo.Numero2
+	fmt.Println(archivo)
+	var result int
+	result = num1 - num2
+	fmt.Fprintf(w, "La resta es %v+%v=%v", num1, num2, result)
+}
+
 func main() {
 
 	fmt.Println("un server papu xd")
@@ -49,8 +67,10 @@ func main() {
 	router.HandleFunc("/", index).Methods("GET")
 	router.HandleFunc("/suma", suma).Methods("POST")
 	router.HandleFunc("/info", info).Methods("GET")
+	router.HandleFunc("/resta", resta).Methods("POST")
 
 
 	log.Fatal(http.ListenAndServe(":3000", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(router)))
 
 }
+
